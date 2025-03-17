3af641dfd2f4c7a1a1be12e740d1128afc7b220f
Hemnath M <146712643+HEMNATH77@users.noreply.github.com>
2025-03-17 22:07:54 +0530

    Update Pattern9.java

--- Pattern9.java
+++ Pattern9.java
@@ -3,18 +3,19 @@ public static void main(String[] args){
 
 int n = 5;
 
-for(int i = 1;i<=n;i++){
-for(int j = 1;j<=i;j++){
-System.out.print("{");
-for(int k = 1; k<j;k++){
-System.out.print("{");
-}
-if ( j > 1){
-System.out.print("}");
-}
-}
-System.out.print("}");
-System.out.println();
-}
+for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("{");
+                for (int k = 1; k < j; k++) {
+                    System.out.print("{");
+                }
+                for (int k = 1; k < j; k++) {
+                    System.out.print("}");
+                }
+                System.out.print("} ");
+            }
+            System.out.println();
+        }
+
 }
 }
